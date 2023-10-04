cb6ffb1644e1277bb06234aed3a31ce48ea0a8d6
Asadul Al Galib <asadulgalibvat69@gmail.com>
2023-10-04 11:26:30 +0600

    commandline arguments

--- start.s
+++ start.s
@@ -1,18 +1,18 @@
-\.intel_syntax noprefix
+.intel_syntax noprefix
 .section .note.GNU-stack,"",@progbits
 
 .text  
-    .global syscall5     
+	.global syscall5     
 
-    syscall5:
-        mov rax, rdi
-        mov rdi, rsi
-        mov rsi, rdx
-        mov rdx, rcx
-        mov r10, r8
-        mov r8, r9
-        syscall
-        ret
+	syscall5:
+		mov rax, rdi
+		mov rdi, rsi
+		mov rsi, rdx
+		mov rdx, rcx
+		mov r10, r8
+		mov r8, r9
+		syscall
+		ret
 
 	.global _start
 	_start:
